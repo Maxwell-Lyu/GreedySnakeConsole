90364321a565e03551d46a3a02f3d288b5c7f7a6
Maxwell Lyu <xlworkshop.chs@hotmail.com>
2020-03-02 22:02:33 +0800

    add move, add color

--- Snake.cpp
+++ Snake.cpp
@@ -1,5 +1,6 @@
 #include "Snake.h"
-Snake::Snake(Render& render, Direction d, int x, int y, int len): render(render), direction(d) {
+
+Snake::Snake(Render& render, Direction d, int x, int y, int len, Color color): render(render), direction(d), color(color) {
   switch(d) {
     case UP: for(int i = 0; i < len; i++) this->points.emplace_back(std::make_pair(x, y + i)); break;
     case LT: for(int i = 0; i < len; i++) this->points.emplace_back(std::make_pair(x + i, y)); break;
@@ -11,5 +12,20 @@ Snake::Snake(Render& render, Direction d, int x, int y, int len): render(render)
 
 void Snake::Print() {
   for(std::deque<std::pair<int,int>>::iterator it = this->points.begin(); it != this->points.end(); ++it)
-    this->render.Draw( std::get<0>(*it), std::get<1>(*it), it == this->points.begin() ? "██" : "▒▒", Color::GRN);
+    this->render.Draw( std::get<0>(*it), std::get<1>(*it), it == this->points.begin() ? "██" : "▒▒", this->color);
+}
+
+void Snake::Move() {
+  std::pair<int,int> to_add, to_fad, to_del;
+  switch(this->direction) {
+    case Direction::UP: { to_fad = this->points.front(); to_del = this->points.back(); to_add = std::make_pair(std::get<0>(to_fad), std::get<1>(to_fad) - 1); break; }
+    case Direction::LT: { to_fad = this->points.front(); to_del = this->points.back(); to_add = std::make_pair(std::get<0>(to_fad) - 1, std::get<1>(to_fad)); break; }
+    case Direction::DN: { to_fad = this->points.front(); to_del = this->points.back(); to_add = std::make_pair(std::get<0>(to_fad), std::get<1>(to_fad) + 1); break; }
+    case Direction::RT: { to_fad = this->points.front(); to_del = this->points.back(); to_add = std::make_pair(std::get<0>(to_fad) + 1, std::get<1>(to_fad)); break; }
+  }
+  this->render.Draw(std::get<0>(to_add), std::get<1>(to_add), "██", this->color);
+  this->render.Draw(std::get<0>(to_fad), std::get<1>(to_fad), "▒▒", this->color);
+  this->render.Draw(std::get<0>(to_del), std::get<1>(to_del), "  ", this->color);
+  this->points.emplace_front(to_add);
+  this->points.pop_back();
 }
--- Snake.h
+++ Snake.h
@@ -6,10 +6,13 @@ enum Direction {UP, LT, DN, RT};
 
 class Snake {
   public:
-    Snake(Render& render, Direction d = Direction::UP, int x = 10, int y = 10, int len = 3);
+    Snake(Render& render, Direction d = Direction::UP, int x = 10, int y = 10, int len = 3, Color color = Color::GRN);
     void Print();
+    void setDirection(Direction d) { this->direction = d; }
+    void Move();
   private:
     Render &render;
     std::deque<std::pair<int,int>> points;
     Direction direction;
+    const Color color;
 };
