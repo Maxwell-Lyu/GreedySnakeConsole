bc93a3e6f41e8a103b90615b93032b4e0df3f4ca
Maxwell Lyu <xlworkshop.chs@hotmail.com>
2020-03-04 10:02:24 +0800

    bounding is now inside snake

--- Snake.cpp
+++ Snake.cpp
@@ -1,12 +1,15 @@
-#include "Snake.h"
+#include "Game.h"
 
-Snake::Snake(Render& render, Direction d, int x, int y, int len, Color color): render(render), direction(d), color(color) {
+Snake::Snake(std::pair<int, int> xBound, std::pair<int, int> yBound, Direction d, int x, int y, int len, Color color): direction(d), color(color) {
   switch(d) {
     case UP: for(int i = 0; i < len; i++) this->points.emplace_back(std::make_pair(x, y + i)); break;
     case LT: for(int i = 0; i < len; i++) this->points.emplace_back(std::make_pair(x + i, y)); break;
     case DN: for(int i = 0; i < len; i++) this->points.emplace_back(std::make_pair(x, y - i)); break;
     case RT: for(int i = 0; i < len; i++) this->points.emplace_back(std::make_pair(x - i, y)); break;
   }
+  this->Print();
+  this->xBound = xBound;
+  this->yBound = yBound;
 }
 
 
@@ -15,19 +18,53 @@ void Snake::Print() {
     this->render.Draw( std::get<0>(*it), std::get<1>(*it), it == this->points.begin() ? "██" : "▒▒", this->color);
 }
 
-void Snake::Move(bool getFood) {
+void Snake::setDirection(Direction d) {
+  if((this->direction ^ d) & 1)
+    this->direction = d; 
+}
+
+
+
+
+int Snake::Move(bool getFood) {
   std::pair<int,int> to_add, to_fad, to_del;
   switch(this->direction) {
-    case Direction::UP: { to_fad = this->points.front(); to_del = this->points.back(); to_add = std::make_pair(std::get<0>(to_fad), std::get<1>(to_fad) - 1); break; }
-    case Direction::LT: { to_fad = this->points.front(); to_del = this->points.back(); to_add = std::make_pair(std::get<0>(to_fad) - 1, std::get<1>(to_fad)); break; }
-    case Direction::DN: { to_fad = this->points.front(); to_del = this->points.back(); to_add = std::make_pair(std::get<0>(to_fad), std::get<1>(to_fad) + 1); break; }
-    case Direction::RT: { to_fad = this->points.front(); to_del = this->points.back(); to_add = std::make_pair(std::get<0>(to_fad) + 1, std::get<1>(to_fad)); break; }
+    case Direction::UP: { 
+      to_fad = this->points.front(); 
+      to_del = this->points.back(); 
+      to_add = justifyPoint({std::get<0>(to_fad), std::get<1>(to_fad) - 1}, this->xBound, this->yBound); 
+      break; }
+    case Direction::LT: { 
+      to_fad = this->points.front(); 
+      to_del = this->points.back(); 
+      to_add = justifyPoint({std::get<0>(to_fad) - 1, std::get<1>(to_fad)}, this->xBound, this->yBound); 
+      break; }
+    case Direction::DN: { 
+      to_fad = this->points.front(); 
+      to_del = this->points.back(); 
+      to_add = justifyPoint({std::get<0>(to_fad), std::get<1>(to_fad) + 1}, this->xBound, this->yBound); 
+      break; }
+    case Direction::RT: { 
+      to_fad = this->points.front(); 
+      to_del = this->points.back(); 
+      to_add = justifyPoint({std::get<0>(to_fad) + 1, std::get<1>(to_fad)}, this->xBound, this->yBound); 
+      break; }
   }
   this->render.Draw(std::get<0>(to_add), std::get<1>(to_add), "██", this->color);
   this->render.Draw(std::get<0>(to_fad), std::get<1>(to_fad), "▒▒", this->color);
   this->points.emplace_front(to_add);
-  if(!getFood) {
-    this->render.Draw(std::get<0>(to_del), std::get<1>(to_del), "  ", this->color);
-    this->points.pop_back();
+  switch(this->foods.checkFood(to_add)) {
+    case EMPTY: {
+      this->render.Draw(std::get<0>(to_del), std::get<1>(to_del), "  ", this->color);
+      this->points.pop_back();
+      return EMPTY;
+    }
+    case BONUS: return BONUS;
+    case FOOD: this->foods.setFood(this->points, xBound, yBound); return FOOD;
   }
+}
+
+
+Snake::~Snake() {
+  this->points.clear();
 }
