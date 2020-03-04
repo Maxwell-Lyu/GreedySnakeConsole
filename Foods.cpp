47940fdf4c26c4cb156588801e43687510ac2f4f
Maxwell Lyu <xlworkshop.chs@hotmail.com>
2020-03-04 10:03:03 +0800

    add food class

---
+++ Foods.cpp
@@ -0,0 +1,28 @@
+#include "Game.h"
+
+std::pair<int,int> Foods::setFood(std::deque<std::pair<int,int>> &avoid, std::pair<int, int> &xBound, std::pair<int, int> &yBound, bool isBonus) {
+  std::pair<int,int> tmp;
+  do {
+    tmp = justifyPoint({rand(), rand()}, {xBound.first + 1, xBound.second - 1}, {yBound.first + 1, yBound.second - 1});
+  } while (tmp == this->bonus || tmp == this->food);
+  if(isBonus) {
+    this->render.Draw(std::get<0>(tmp), std::get<1>(tmp), "ボ", Color::YLW);
+    this->bonus = tmp;
+  }
+  else {
+    this->render.Draw(std::get<0>(tmp), std::get<1>(tmp), "フ", Color::MAG);
+    this->food = tmp;
+  }
+  return tmp;
+} 
+
+int Foods::checkFood(std::pair<int, int> &target) {
+  if(target == this->food) return FOOD;
+  if(target == this->bonus) return BONUS;
+  return EMPTY;
+}
+
+void Foods::dropBonus() {
+  this->render.Draw(this->bonus.first, this->bonus.second, "[]", Color::BLK);
+  this->bonus = {-1,-1};
+}
